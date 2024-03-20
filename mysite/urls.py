549571975bbb4f9ca3bca74ce148af02d12f811d
leonardonels <goldenmatrix2.0@gmail.com>
2024-03-20 10:02:32 +0100

    added OTP

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     #path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('login/', login_view, name='login'),
+    path('otp/', otp, name='otp'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from media.models import image_link
 from users.forms import RegistrationForm
 from django.contrib.auth import login, logout, authenticate
+from .utils import send_otp
+from datetime import datetime
+from users.models import User 
+import pyotp
 
 def home(request):
     images = image_link.objects.all()
@@ -29,12 +33,44 @@ def login_view(request):
         if user is not None:
             #send one time password
             #...
-            login(request, user)
-            #return redirect('otp')
-            return redirect('home')
+            send_otp(request)
+            request.session['username']=username
+            return redirect('otp')
+            
+            #login(request, user)
+            #return redirect('home')
         else:
             error_message = 'invalid username or password'
     return render(request, 'login.html', {'error_message':error_message})
 
-#def otp(request):
-#    return render(request, 'otp.html', {})
+def otp(request):
+    error_message=None
+    if request.method == 'POST':
+        otp = request.POST['otp']
+        username = request.session['username'] 
+
+        otp_secret_key = request.session['otp_secret_key']
+        otp_valid_date = request.session['otp_valid_date']
+
+        if otp_secret_key and otp_valid_date is not None:
+            valid_date=datetime.fromisoformat(otp_valid_date)
+
+            if valid_date > datetime.now():
+                 totp = pyotp.TOTP(otp_secret_key, interval=60)
+
+                 if totp.verify(otp):
+                    user=get_object_or_404(User, username=username)
+                    login(request, user)
+
+                    del request.session['otp_secret_key']
+                    del request.session['otp_valid_date']
+
+                    return redirect('home')
+                 else:
+                     error_message="invalid one time password"
+            else:
+                error_message="one time password has expired"
+        else:
+            error_message="ops...something went wrong :("
+     
+    return render(request, 'otp.html', {'error_message':error_message})
