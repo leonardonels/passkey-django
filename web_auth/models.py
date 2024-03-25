a505cd3f8b5ce67e3dd2b1e2c70a592635302a4a
leonardonels <goldenmatrix2.0@gmail.com>
2024-03-26 00:45:05 +0100

    small improvements part 3 la vendetta

--- web_auth/models.py
+++ web_auth/models.py
@@ -1,11 +1,43 @@
 from django.db import models
 from users.models import User
+from webauthn.helpers.structs import AuthenticatorTransport
+import uuid, json, base64
 
 # Create your models here.
 
 class Credential(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='credentials')
+    id_bytes = models.BinaryField(unique=True, editable=False)
     credential_id = models.BinaryField()
     public_key = models.BinaryField()
     sign_counts = models.IntegerField()
     transports = models.CharField(max_length=255)
+
+    def set_transports(self, transports):
+        #enum list to str list
+        enum_string_list = [transport.value for transport in transports]
+        #str list to json
+        enum_json_string = json.dumps(enum_string_list)
+        #json to base64 to str
+        self.transports = base64.b64encode(enum_json_string.encode('utf-8')).decode('utf-8')
+
+        self.save()
+
+    def get_transports(self):
+        #str to base64 to json
+        decoded_bytes = base64.b64decode(self.transports.encode('utf-8'))
+        #decode json
+        decoded_json_string = decoded_bytes.decode('utf-8')
+        #json to str list
+        enum_string_list = json.loads(decoded_json_string)
+        #str list to enum list
+        transports_enum_list = [AuthenticatorTransport(transport) for transport in enum_string_list]
+
+        return transports_enum_list
+
+    transports_list = property(get_transports, set_transports)
+
+    def save(self, *args, **kwargs):
+        if not self.id_bytes:
+            self.id_bytes = uuid.uuid4().bytes
+        return super().save(*args, **kwargs)
--- web_auth/views.py
+++ web_auth/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from users.models import User
 from webauthn import generate_registration_options, verify_registration_response, generate_authentication_options, verify_authentication_response
-from webauthn.helpers.structs import AttestationConveyancePreference, UserVerificationRequirement
+from webauthn.helpers.structs import AttestationConveyancePreference, UserVerificationRequirement, PublicKeyCredentialDescriptor, PublicKeyCredentialType
 from webauthn.helpers.options_to_json import options_to_json
 from webauthn.helpers.parse_registration_credential_json import parse_registration_credential_json
 from webauthn.helpers.parse_authentication_credential_json import parse_authentication_credential_json
 from mysite.settings import REPLYING_PARTY_ID, REPLYING_PARY_NAME, ORIGIN
 from django.http import JsonResponse
-import json, secrets, struct
+import json, secrets, struct, base64
 from rest_framework.views import APIView
 from .models import Credential
 from django.core.cache import cache
 from django.views.decorators.csrf import csrf_exempt
+from typing import List
+
 
 # Create your views here.
 
@@ -61,8 +63,8 @@ def registration_verification(request):
         credential_id=verification.credential_id,
         public_key=verification.credential_public_key,
         sign_counts=verification.sign_count,
-        transports=credential.response.transports
     )
+    new_credential.set_transports(credential.response.transports)
 
     return JsonResponse({"verified":True})
 
@@ -70,29 +72,40 @@ def registration_verification(request):
 def authentication(request):
     username = request.session['username']
     user=get_object_or_404(User, username=username)
+    allow_credentials: List[PublicKeyCredentialDescriptor] = []
+
+    for cred in user.credentials.all():
+        allow_credentials.append(
+            PublicKeyCredentialDescriptor(
+                id=cred.id_bytes,
+                type=PublicKeyCredentialType.PUBLIC_KEY,
+                transports=cred.get_transports()
+            )
+        )
+
     options=generate_authentication_options(
         rp_id=REPLYING_PARTY_ID,
-        allow_credentials=[
-            {"id":cred.id, "type":"public-key", "transports":cred.transports}
-            for cred in user.credentials.all()
-        ],
+        allow_credentials=allow_credentials,
     )
 
     #create and save the challenge
     challenge=options.challenge
-    cache.set(f'challenge_{user.id}', challenge, timeout=300)
+    cache.set(f'challenge', challenge, timeout=300)
+    print(cache.get(f'challenge_{user.id}'))
 
     return JsonResponse(json.loads(options_to_json(options)))
 
 
 def authentication_verification(request):
     request_body=request.body.decode('utf-8')
+    data = json.loads(request_body)
+    username = data.get('username', None)
+    user=get_object_or_404(User, username=username)
 
+    #print(cache.get(f'challenge_{user.id}'))
+    
     try:
         request_credential=parse_authentication_credential_json(request_body)
-        data = json.loads(request_body)
-        username = data.get('username', None)
-        user=get_object_or_404(User, username=username)
         user_credential = Credential.objects.filter(user_id=user.id, id=request_credential.raw_id).first()
         print(user_credential)
 
