e6658be8b212f20df0a0e8b53ff3e5bf8db0929d
leonardonels <goldenmatrix2.0@gmail.com>
2024-03-25 21:19:47 +0100

    small improvements part 2

--- web_auth/urls.py
+++ web_auth/urls.py
@@ -6,5 +6,9 @@ app_name = "web_auth"
 urlpatterns = [
     path('registration/', registration, name='registration'),
     path('registration_verification/', registration_verification, name='registration_verification'),
+    path('authentication/', authentication, name='authentication'),
+    path('authentication_verification/', authentication_verification, name='authentication_verification'),
     path('remove_passkey/', remove_passkey, name='remove_passkey'),
+    path('login_with_passkey/', login_with_passkey, name='login_with_passkey'),
+    path('login_with_passkey/set_username/', set_username_in_session, name='set_username'),
 ]
--- web_auth/views.py
+++ web_auth/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from users.models import User
-from webauthn import generate_registration_options, verify_registration_response
-from webauthn.helpers.structs import AttestationConveyancePreference, RegistrationCredential
+from webauthn import generate_registration_options, verify_registration_response, generate_authentication_options, verify_authentication_response
+from webauthn.helpers.structs import AttestationConveyancePreference, UserVerificationRequirement
 from webauthn.helpers.options_to_json import options_to_json
 from webauthn.helpers.parse_registration_credential_json import parse_registration_credential_json
+from webauthn.helpers.parse_authentication_credential_json import parse_authentication_credential_json
 from mysite.settings import REPLYING_PARTY_ID, REPLYING_PARY_NAME, ORIGIN
 from django.http import JsonResponse
-import json, secrets
+import json, secrets, struct
 from rest_framework.views import APIView
 from .models import Credential
 from django.core.cache import cache
@@ -34,6 +35,7 @@ def registration(request):
     cache.set(f'challenge_{user.id}', challenge, timeout=300)
 
     return JsonResponse(json.loads(options_to_json(options)))
+
     
 def registration_verification(request):
 
@@ -42,18 +44,7 @@ def registration_verification(request):
 
     #start registration flow
     request_body=request.body.decode('utf-8')
-    print(request_body)
-    request_data = json.loads(request_body)
-
     try:
-        id=request_data.get('id')
-        raw_id = request_data.get('rawId')
-        response = request_data.get('response')
-
-        print(type(id))
-        print(type(raw_id))
-        print(type(response))
-
         credential=parse_registration_credential_json(request_body)
         verification=verify_registration_response(
             credential=credential,
@@ -75,6 +66,70 @@ def registration_verification(request):
 
     return JsonResponse({"verified":True})
 
+
+def authentication(request):
+    username = request.session['username']
+    user=get_object_or_404(User, username=username)
+    options=generate_authentication_options(
+        rp_id=REPLYING_PARTY_ID,
+        allow_credentials=[
+            {"id":cred.id, "type":"public-key", "transports":cred.transports}
+            for cred in user.credentials.all()
+        ],
+    )
+
+    #create and save the challenge
+    challenge=options.challenge
+    cache.set(f'challenge_{user.id}', challenge, timeout=300)
+
+    return JsonResponse(json.loads(options_to_json(options)))
+
+
+def authentication_verification(request):
+    request_body=request.body.decode('utf-8')
+
+    try:
+        request_credential=parse_authentication_credential_json(request_body)
+        data = json.loads(request_body)
+        username = data.get('username', None)
+        user=get_object_or_404(User, username=username)
+        user_credential = Credential.objects.filter(user_id=user.id, id=request_credential.raw_id).first()
+        print(user_credential)
+
+        if not user_credential:
+            raise Exception("User does not have credentials with give ID")
+        
+        verification=verify_authentication_response(
+            Credential=request_credential,
+            expected_challenge=cache.get(f'challenge_{user.id}'),
+            expected_rp_id=REPLYING_PARTY_ID,
+            expected_origin=ORIGIN,
+            credential_public_key=user_credential.public_key,
+            credential_current_sign_count=user_credential.sign_counts,
+            require_user_verification=True,
+        )
+    except Exception as err:
+        return JsonResponse({"verified":False, "msg":str(err), "status":400})
+    
+    user_credential.sign_counts=verification.new_sign_count
+    return JsonResponse({"verified":True})
+
+
+
 def remove_passkey(request):
     Credential.objects.filter(user=request.user).delete()
     return redirect('/users/profile')
+
+
+def login_with_passkey(request):
+    return render(request, 'login_with_passkey.html')
+
+def set_username_in_session(request):
+    if request.method == 'POST':
+        request_data=json.loads(request.body)
+        username = request_data.get('username')
+        if username:
+            request.session['username'] = username
+            request.session.modified = True
+            return JsonResponse({'success': True})
+    return JsonResponse({'success': False}, status=400)
